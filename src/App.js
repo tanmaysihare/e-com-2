989efde17eaa67970b8d34bb3618711404c457ae
tanmaysihare <tanmaysihare@gmail.com>
2023-09-20 06:35:35 +0530

    retsetting

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+
+import Menu from "./Components/Home/Layout/Menu";
+import { Route,Switch,Redirect } from "react-router-dom";
+import About from "./Components/About/About";
+import Home from "./Components/Home/Home";
+import Contact from "./Components/ContactUS/Contact";
+import ProductsDetail from "./Components/Home/Products Detail/ProductDetail";
+
+
 
 function App() {
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      
+        <Menu/>
+        
+          <Switch>
+          
+            <Route path="/" exact><Redirect to='/' /></Route>
+            
+            <Route path="/home"><Home /></Route>
+            <Route path="/about"><About /></Route>
+            <Route path="/contact"><Contact /></Route>
+            <Route path="/home/:productDetailId"><ProductsDetail /> </Route>
+        
+          </Switch>
+        
+    
+      </>
   );
 }
 
